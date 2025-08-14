e36ce46f73de273632c82816617a6592a9506607
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-08-14 21:29:56 +0530

    implementations

---
+++ implementations/divide-and-conquer/mergesort.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+
+using std::vector;
+
+template <typename T>
+void merge(vector<T>& v, int l, int r, int mid, vector<T>& aux) {
+	int i = l, j = mid + 1, k = l;
+	while (i <= mid && j <= r) {
+		if (v[i] <= v[j]) {  aux[k++] = v[i++];  }
+		else 			  {  aux[k++] = v[j++];  }
+	}
+
+	while (i <= mid) {  aux[k++] = v[i++];  }
+	while (j <= r)   {  aux[k++] = v[j++];  }
+	for(int ldx = l; ldx <= r; ldx++) {
+		v[ldx] = aux[ldx];
+	}
+}
+
+template <typename T>
+void mergeSortRecursive(vector<T>& v, int l, int r, vector<T>& aux) {
+	if (l >= r) return;
+	int mid = l + (r - l) / 2;
+	mergeSortRecursive(v, l, mid, aux);
+	mergeSortRecursive(v, mid + 1, r, aux);
+	merge(v, l, r, mid, aux);
+}
+
+template <typename T>
+void sort(vector<T>& v) {
+	if (v.empty())	return;
+	int l = 0, h = (int) v.size() - 1;
+	vector<T> aux(v.size());
+	mergeSortRecursive(v, l, h, aux);
+}
+
+int main() {
+	
+}
---
+++ implementations/divide-and-conquer/quicksort.cpp
@@ -0,0 +1,47 @@
+#include <random>
+#include <vector>
+
+using std::vector;
+using std::swap;
+
+template <typename T>
+int splitArray(vector<T>& A, int lo, int hi) {
+	// generate a random index
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> dist(lo, hi);
+	int idx = dist(gen);
+
+	swap(A[lo], A[idx]);	// swap with lo
+
+	// partition the array around the pivot
+	int i = lo + 1;
+	T pivot = A[lo];	// pivot is at index lo.
+	for(int j = lo + 1; j <= hi; j++){
+		if (A[j] < pivot){
+			swap(A[j], A[i++]);
+		}
+	}
+	swap(A[i - 1], A[lo]);
+	
+	return (i - 1);
+}
+
+template <typename T>
+void QuickSort(vector<T>& A, int lo, int hi) {
+	if (lo >= hi)	return;
+	int idx = splitArray(A, lo, hi);
+	QuickSort(A, lo, idx - 1);
+	QuickSort(A, idx + 1, hi);
+}
+
+template <typename T>
+void sort(vector<T>& A) {
+	if (A.empty())	return;
+	int lo = 0, hi = (int) A.size() - 1;
+	QuickSort(A, lo, hi);
+}
+
+int main() {
+
+}
