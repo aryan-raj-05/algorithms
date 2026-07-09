da0ab068b01098497bd7b7c9c2c529fe0931756b
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2026-07-09 21:17:07 +0530

    update: Queue to use circular array
    instead of linked list

--- data_structures/Queue.java
+++ data_structures/Queue.java
@@ -2,33 +2,70 @@ package data_structures;
 
 import java.util.NoSuchElementException;
 
-public class Queue<T> {
-    private LinkedList<T> list;
+public class Queue {
+    private int[] q;
+    private int head;
+    private int tail;
+    private int size;
+    private int capacity;
 
     public Queue() {
-        list = new LinkedList<>();
+        q = new int[8];
+        capacity = 8;
+        head = tail = 0;
+        size = 0;
     }
 
-    public void enqueue(T item) {
-        list.addLast(item);
+    public void enqueue(int item) {
+        if (isFull()) resize();
+
+        q[tail] = item;
+        tail = next(tail);
+        size++;
     }
 
-    public T dequeue() {
+    public int dequeue() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
 
-        return list.removeFirst();
+        int temp = q[head];
+        head = next(head);
+        size--;
+        return temp;
     }
 
-    public T peek() {
+    public int peek() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
-        return list.get(0);
+        return q[head];
     }
 
     public boolean isEmpty() {
-        return list.isEmpty();
+        return size == 0;
+    }
+
+    private boolean isFull() {
+        return size == capacity;
+    }
+
+    private int next(int x) {
+        return (x + 1) % capacity;
+    }
+
+    private void resize() {
+        int[] temp = q;
+        int oldCapacity = capacity;
+
+        capacity *= 2;
+        q = new int[capacity];
+
+        for (int i = 0; i < size; i++) {
+            q[i] = temp[(head + i) % oldCapacity];
+        }
+
+        head = 0;
+        tail = size;
     }
 }
